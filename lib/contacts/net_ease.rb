89af10fc2efe62d9088922965b202fb723475eb3
fushang318 <fushang318@gmail.com>
2011-03-01 18:19:44 -0800

    网易接口调整了

--- lib/contacts/net_ease.rb
+++ lib/contacts/net_ease.rb
@@ -102,7 +102,7 @@ class Contacts
 			#get mail server and sid
 			enter_mail_url = ENTER_MAIL_URL[@mail_type] % @login
 			data, resp, cookies, forward = get(enter_mail_url,@cookies)
-			unless data.match(/<a.*?(http.*?)main.jsp\?sid=(.*?)\">/)
+                        unless forward.match(/(http.*)main.jsp\?sid=(.*)?/)
 				raise ConnectionError, self.class.const_get(:PROTOCOL_ERROR)
 			end
 			@cookies = cookies
